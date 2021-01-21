8c8e22d67d40287710ee13dd75e61ab5caa1421d
Allan Kildare <allankildare27@gmail.com>
2021-01-21 01:18:36 -0300

    modal via request alterada

--- scripts/modalRequest.js
+++ scripts/modalRequest.js
@@ -1,17 +1,3 @@
-// adicionando eventos
-document.getElementById('filme1').addEventListener('click', infoFilme('Avengers: Endgame', 1))
-document.getElementById('filme2').addEventListener('click', infoFilme('The Boys', 2))
-document.getElementById('filme3').addEventListener('click', infoFilme('Fleabag', 3))
-document.getElementById('filme4').addEventListener('click', infoFilme('Life Itself', 4))
-document.getElementById('filme5').addEventListener('click', infoFilme('Merli', 5))
-document.getElementById('filme6').addEventListener('click', infoFilme('Pride and Prejudice', 6))
-document.getElementById('filme7').addEventListener('click', infoFilme('Shrek', 7))
-document.getElementById('filme8').addEventListener('click', infoFilme('Fantastic Beasts and Where to Find Them', 8))
-document.getElementById('filme9').addEventListener('click', infoFilme('Fight Club', 9))
-document.getElementById('filme10').addEventListener('click', infoFilme('Freedom Writers', 10))
-document.getElementById('filme11').addEventListener('click', infoFilme('How to Get Away with Murder', 11))
-document.getElementById('filme12').addEventListener('click', infoFilme('Hacksaw Ridge', 12))
-
 // requisicao para modal
 async function infoFilme (nomeFilme, numFilme) {
     const key = 'df65174b'
@@ -40,4 +26,17 @@ async function infoFilme (nomeFilme, numFilme) {
         let traducao = document.getElementById(`traducao-${numFilme}`)
         traducao.innerHTML = `<a href="https://translate.google.com.br/?hl=pt-BR&ui=tob&sl=en&tl=pt&text=${resposta.Plot}%0A&op=translate" target="_blank">Traduzir sinopse</a>`
 }    
-    
+
+// adicionando eventos
+document.getElementById('filme1').addEventListener('click', infoFilme('Avengers: Endgame', 1))
+document.getElementById('filme2').addEventListener('click', infoFilme('The Boys', 2))
+document.getElementById('filme3').addEventListener('click', infoFilme('Fleabag', 3))
+document.getElementById('filme4').addEventListener('click', infoFilme('Life Itself', 4))
+document.getElementById('filme5').addEventListener('click', infoFilme('Merli', 5))
+document.getElementById('filme6').addEventListener('click', infoFilme('Pride and Prejudice', 6))
+document.getElementById('filme7').addEventListener('click', infoFilme('Shrek', 7))
+document.getElementById('filme8').addEventListener('click', infoFilme('Fantastic Beasts and Where to Find Them', 8))
+document.getElementById('filme9').addEventListener('click', infoFilme('Fight Club', 9))
+document.getElementById('filme10').addEventListener('click', infoFilme('Freedom Writers', 10))
+document.getElementById('filme11').addEventListener('click', infoFilme('How to Get Away with Murder', 11))
+document.getElementById('filme12').addEventListener('click', infoFilme('Hacksaw Ridge', 12))
